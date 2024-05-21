0cc643e3f198e1b20e08bceccc836323129c59a2
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-05-21 18:41:22 +0300

    JS-Lab

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// 1-حساب مساحة المستطيل
+
+let recH=2
+let recW=4
+const R=(recH*recW)
+let message = `The area of the rectangle is:` + R;
+console.log(message)
+
+// ==================================================================
+
+// 2-محول درجة الحرارة
+let C=15
+let F= (9/5)*C+32
+console.log(`${F} F° is ${C} C°
+`);
+
+// ==================================================================
+// 3-من ساعة الى ثانية
+
+hour = 2
+seconds = hour * 60 * 60
+console.log(`${hour} hours = ${seconds} Seconds
+`);
